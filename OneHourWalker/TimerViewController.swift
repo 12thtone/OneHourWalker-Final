3e13f51b7f2aafdf55d52aa999e2cadebc498641
Matt Maher <maher@12thtone.com>
2016-02-23 22:09:56 -0500

    Comments and bugs

--- OneHourWalker/TimerViewController.swift
+++ OneHourWalker/TimerViewController.swift
@@ -8,45 +8,39 @@
 
 import UIKit
 import CoreLocation
+import HealthKit
 
 class TimerViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var milesLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
     
     var zeroTime = NSTimeInterval()
     var timer : NSTimer = NSTimer()
     
     let locationManager = CLLocationManager()
-    var currentLocation: CLLocation!
+    var startLocation: CLLocation!
     var lastLocation: CLLocation!
     var distanceTraveled = 0.0
+    
+    let healthManager:HealthKitManager = HealthKitManager()
+    var height: HKQuantitySample?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        locationManager.requestWhenInUseAuthorization();
+        locationManager.requestWhenInUseAuthorization()
         
         if CLLocationManager.locationServicesEnabled(){
             locationManager.delegate = self
-//            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//            locationManager.startUpdatingLocation()
-            
-            distanceTraveled = 0.0
-            currentLocation = nil
-            lastLocation = nil
-            
+        } else {
+            print("Need to Enable Location")
         }
-        else {
-            print("Location service disabled");
-        }
-    }
-    
-    override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(animated)
         
-        locationManager.stopUpdatingLocation()
+        // We cannot access the user's HealthKit data without specific permission.
+        getHealthKitPermission()
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,58 +48,53 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
 
     }
     
-    @IBAction func startTimer(sender: AnyObject) {
-        if (!timer.valid) {
-            let aSelector : Selector = "updateTime"
-            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
-            zeroTime = NSDate.timeIntervalSinceReferenceDate()
-            
-            distanceTraveled = 0.0
-            currentLocation = nil
-            lastLocation = nil
-            
-            locationManager.startUpdatingLocation()
+    func getHealthKitPermission() {
+        
+        // Seek authorization in HealthKitManager.swift.
+        healthManager.authorizeHealthKit { (authorized,  error) -> Void in
+            if authorized {
+                
+                // Get and set the user's height.
+                self.setHeight()
+            } else {
+                if error != nil {
+                    print(error)
+                }
+                print("Permission denied.")
+            }
         }
+    }
+    
+    @IBAction func startTimer(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+        zeroTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        distanceTraveled = 0.0
+        startLocation = nil
+        lastLocation = nil
+        
+        locationManager.startUpdatingLocation()
     }
     
     @IBAction func stopTimer(sender: AnyObject) {
         timer.invalidate()
         locationManager.stopUpdatingLocation()
-        distanceTraveled = 0.0
-        currentLocation = nil
-        lastLocation = nil
-    }
-    
-    @IBAction func share(sender: AnyObject) {
-        
     }
     
     func updateTime() {
         let currentTime = NSDate.timeIntervalSinceReferenceDate()
-        
-        //
-        //Find the difference between current time and start time.
-        var elapsedTime: NSTimeInterval = currentTime - zeroTime
-        
-        //calculate the minutes in elapsed time.
-        let minutes = UInt8(elapsedTime / 60.0)
-        elapsedTime -= (NSTimeInterval(minutes) * 60)
-        
-        //calculate the seconds in elapsed time.
-        let seconds = UInt8(elapsedTime)
-        elapsedTime -= NSTimeInterval(seconds)
-        
-        //find out the fraction of milliseconds to be displayed.
-        let fraction = UInt8(elapsedTime * 100)
-        
-        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        var timePassed: NSTimeInterval = currentTime - zeroTime
+        let minutes = UInt8(timePassed / 60.0)
+        timePassed -= (NSTimeInterval(minutes) * 60)
+        let seconds = UInt8(timePassed)
+        timePassed -= NSTimeInterval(seconds)
+        let millisecsX10 = UInt8(timePassed * 100)
         
         let strMinutes = String(format: "%02d", minutes)
         let strSeconds = String(format: "%02d", seconds)
-        let strFraction = String(format: "%02d", fraction)
+        let strMSX10 = String(format: "%02d", millisecsX10)
         
-        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
-        timerLabel.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+        timerLabel.text = "\(strMinutes):\(strSeconds):\(strMSX10)"
         
         if timerLabel.text == "60:00:00" {
             timer.invalidate()
@@ -114,25 +103,53 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if currentLocation == nil {
-            currentLocation = locations.first as CLLocation!
-            lastLocation = locations.last as CLLocation!
+        if startLocation == nil {
+            startLocation = locations.first as CLLocation!
         } else {
-            let distance = currentLocation.distanceFromLocation(locations.last as CLLocation!)
             let lastDistance = lastLocation.distanceFromLocation(locations.last as CLLocation!)
             distanceTraveled += lastDistance * 0.000621371
             
             let trimmedDistance = String(format: "%.2f", distanceTraveled)
             
-            print( "\(currentLocation)")
-            print( "\(locations.last!)")
-            print("FULL DISTANCE: \(trimmedDistance)")
-//            print("FULL DISTANCE MILES: \(distanceTraveled)")
-            print("STRAIGHT DISTANCE: \(distance)")
-            
             milesLabel.text = "\(trimmedDistance) Miles"
         }
+        
         lastLocation = locations.last as CLLocation!
+    }
+    
+    func setHeight() {
+        // Create the HKSample for Height.
+        let heightSample = HKSampleType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeight)
+        
+        // Call HealthKitManager's getSample() method to get the user's height.
+        self.healthManager.getSample(heightSample!, completion: { (userHeight, error) -> Void in
+            
+            if( error != nil ) {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            
+            var heightString = ""
+            
+            self.height = userHeight as? HKQuantitySample
+            
+            // The height is formatted to the user's locale.
+            if let meters = self.height?.quantity.doubleValueForUnit(HKUnit.meterUnit()) {
+                let formatHeight = NSLengthFormatter()
+                formatHeight.forPersonHeightUse = true
+                heightString = formatHeight.stringFromMeters(meters)
+            }
+            
+            // Set the label to reflect the user's height.
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.heightLabel.text = heightString
+            })
+        })
+        
+    }
+    
+    @IBAction func share(sender: AnyObject) {
+        healthManager.saveDistance(distanceTraveled, date: NSDate())
     }
 
 }
